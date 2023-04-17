baf372cf47262fbd23c1347c8872ef26d02b4288
jocasas <102597441+jocasas@users.noreply.github.com>
2023-04-16 23:55:51 -0400

    Implementando SQL
    npm (mysql)
    npm (mysql2)
    
    Implemente SQL como api consumible, falta refactorizarla a consumir un servicio y despues la funcion que utiliza la query refactorizarla para usarla en cualquier otro tipo de consulta.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
 import Link from 'next/link'
 import Navbar from '@/components/Navbar'
+import { useEffect,useState } from 'react'
 
 export default function Home() {
+  const[dataResponse,setdataResponse] = useState([]);
+  useEffect(() =>{
+    async function getPageData(){
+      const apiUrlEndpoint = 'http://localhost:3000/api/getdata'
+      const response = await fetch(apiUrlEndpoint);
+      const res = await response.json();
+      console.log(res.productos);
+      setdataResponse(res.productos);
+    }
+    getPageData();
+  },[]);
   return (
     <div>
       <Navbar/>
       <h1>CHANCHITO FELIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIZ</h1>
-      <Link href='login'>IR LOGIN</Link>
+      {dataResponse.map((productos)=>{return(<div>nombre: {productos.NOMBRE}</div>)})}
     </div>
   )
 }
