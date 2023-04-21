4f94b11d4389c15ed1e5e2db0d8fedfc74489601
jocasas <102597441+jocasas@users.noreply.github.com>
2023-04-21 05:55:33 -0400

    Implemente transbank
    toma jordan

--- pages/api/getdata.js
+++ pages/api/getdata.js
@@ -35,7 +35,6 @@ export default async function handler(req,res) {
         const values = [];
         const [data] = await dbconnection.execute(query,values);
         dbconnection.end();
-
         res.status(200).json({productos:data});
     } catch(error) {
         //res.status(500).json({error: error.message});
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,40 @@
 import Link from 'next/link'
 import Navbar from '@/components/Navbar'
-import { useEffect,useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
-  const[dataResponse,setdataResponse] = useState([]);
-  useEffect(() =>{
-    async function getPageData(){
+  const [tokenResponse, setToken] = useState([]);
+  const [dataResponse, setdataResponse] = useState([]);
+  useEffect(() => {
+    async function getPageData() {
       const apiUrlEndpoint = 'http://localhost:3000/api/getdata'
       const response = await fetch(apiUrlEndpoint);
       const res = await response.json();
       console.log(res.productos);
       setdataResponse(res.productos);
     }
+
+    async function getToken() {
+      console.log("esta wea esta funcionando o no")
+      const response = await fetch('http://localhost:3000/api/webpay_plus');
+      const res = await response.json()
+      console.log(res.token)
+      setToken(res.token)
+    }
+
     getPageData();
-  },[]);
+    getToken();
+  }, []);
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <h1>CHANCHITO FELIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIZ</h1>
-      {dataResponse.map((productos)=>{return(<div>nombre: {productos.NOMBRE}</div>)})}
+      {dataResponse.map((productos) => { return (<div>nombre: {productos.NOMBRE}</div>) })}
+      <div><form action="https://webpay3gint.transbank.cl/webpayserver/initTransaction" method="POST">
+        <input type="hidden" name="token_ws" value= {tokenResponse} />
+        <input type="submit" value="Pagar" />
+      {tokenResponse}
+      </form></div>
     </div>
   )
 }
