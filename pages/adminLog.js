98f37df65f1e211adda08ea03c2972ad65bd3ba0
Vrivas99 <vi.rivas@duocuc.cl>
2023-04-26 02:38:58 -0400

    adminTemplates

---
+++ pages/adminLog.js
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import React from 'react'
+
+const adminLog = () => {
+    return (
+        <div className='bg-gray-900 h-screen'>
+            <div className='flex justify-center'>
+                <form className='w-1/3 bg-gray-800 p-10 rounded-md mt-64 '>
+                    <div className='flex justify-center m-3'>
+                        <h1 className='font-bold text-white text-2xl'>Admin Login</h1>
+                    </div>
+                    <div className="mb-6">
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Usuario</label>
+                        <input type="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="elchupazorra123" required></input>
+                    </div>
+                    <div className="mb-6">
+                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña</label>
+                        <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required></input>
+                    </div>
+                    <div className='flex justify-center'>
+                        <Link href='adminPage' type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ingresar</Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default adminLog
