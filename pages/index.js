b68f70bd0153de8f54e0b24df558593af8c326c9
jocasas <102597441+jocasas@users.noreply.github.com>
2023-05-02 21:22:47 -0400

    Wrapper para bd mysql

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,22 @@ import Catbanners from '@/components/Catbanners'
 import Navbar from '@/components/Navbar'
 import Prodcards from '@/components/Prodcards'
 import { getItems } from '@/services/itemService'
+import { useEffect, useState } from 'react'
+
 
 export default function Home({items}) {
+  const [dataResponse,setDataresponse]=useState([]);
+
+/*   useEffect(() =>{
+    async function showproducts(){
+      const apiUrlEndpoint = 'http://localhost:3000/api/getdata-lib'
+      const response = await fetch(apiUrlEndpoint);
+      const res = await response.json();
+      setDataresponse(res.productos);
+    }
+    showproducts();
+  },[]);   */
+
   return (
     <div>
       <Navbar/>
@@ -12,7 +26,12 @@ export default function Home({items}) {
         {items && items.map((item)=> (
             <Prodcards key={item.id} item={item} showAs='default'/>
           ))}
-      </div>
+{/*         {dataResponse.map((productos) => {
+          return(
+            <div>{productos.idPRODUCTO}{productos.NOMBRE}</div>
+          );
+        })}
+ */}      </div>
       
     </div>
   )
