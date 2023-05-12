e1780c0b7e41ad60db536a1bb810e0e73d013946
jocasas <102597441+jocasas@users.noreply.github.com>
2023-05-11 22:07:09 -0400

    hola

--- pages/api/mysql/probando.js
+++ pages/api/mysql/probando.js
@@ -1,14 +1,7 @@
 import { pool } from "../../../config/db";
 
 export default async function handler(req,res){
-    const [rows] = await pool.query('SELECT NOW()')
-    const [result] = await pool.query('INSERT INTO PRODUCTOS SET ?', {
-        NOMBRE,
-        DESCRIPCION,
-        PRECIO,
-        CANTIDAD,
-        MARCA_idMARCA,
-    });
-    return res.status(200).json(rows[0]['NOW()']);
+    const [rows] = await pool.query('SELECT * from usuario')
+    return res.status(200).json(rows);
 }
 
