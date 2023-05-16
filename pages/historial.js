e230aabf76f6b397c87082d7a986415b4c9872e7
Marcelo Contreras <marcelocg2002@gmail.com>
2023-05-15 21:35:44 -0400

    modificacion de historial
    se modifico el historial de manera que parezca acordeon

--- pages/historial.js
+++ pages/historial.js
@@ -5,25 +5,36 @@ import Tablahistorial from '@/components/Tablahistorial'
 const historial = () => {
     return (
         <div>
-            <div>
-                <Navbar />
-            </div>
+      <div>
+        <Navbar />
+      </div>
+      
+      <div className='m-11 relative overflow-x-auto sm:rounded-xl'>
+        <table className='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
+          <thead className='text-xs text-gray-700 uppercase bg-purple-500 dark:text-gray-400'>
+            <tr>
+              <th scope='col' className='text-white px-6 py-3 text-center'>
+                PRODUCTOS
+              </th>
+              <th scope='col' className='text-white px-6 py-3 text-center'>
+                PRECIO
+              </th>
+              <th scope='col' className='text-white px-6 py-3 text-center'>
+                FECHA DE COMPRA
+              </th>
+              <th scope='col' className='text-white px-6 py-3 text-center'>
+                Detalles
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+
+            <Tablahistorial />
             
-            <div className='m-11 relative overflow-x-auto sm:rounded-xl'>
-                <table className='w-full  text-sm text-left text-gray-500 dark:text-gray-400'>
-                    <thead className='text-xs text-gray-700 uppercase bg-purple-500 dark:text-gray-400'>
-                        <tr className=''>
-                            <th scope='col' className='text-white px-6 py-3 text-center'>PRODUCTOS</th>
-                            <th scope='col' className='text-white px-6 py-3 text-center'>CANTIDAD PRODUCTOS</th>
-                            <th scope='col' className='text-white px-6 py-3 text-center'>PRECIO</th>
-                            <th scope='col' className='text-white px-6 py-3 text-center'>FECHA DE COMPRA</th>
-                        </tr>
-                    </thead>
-                        <Tablahistorial />
-                    </table>
-            </div>
-            
-        </div>
+          </tbody>
+        </table>
+      </div>
+    </div>
     )
 }
 
