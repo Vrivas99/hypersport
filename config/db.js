609ef040ff97d0e3e6268aeeda9c31fb17378c5c
jocasas <102597441+jocasas@users.noreply.github.com>
2023-06-17 18:13:51 -0400

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,11 @@
 import { createPool } from "mysql2/promise";
+
 //archivo para configurar el packete de mysql2 con la opcion pool en documentacion
 const pool = createPool({
   host: "localhost",
   user: "root",
-  password: "3nrNZphxT(yd",
+  password: process.env.MYSQL_PWD,
+  //hola debes crearte tu archivo {.env.local} en la carpeta 'root' y hay crear tu variable MYSQL_PWD = 'con tu clave'(ej: 20173)
   database: "test",
   port: 3306,
 });
