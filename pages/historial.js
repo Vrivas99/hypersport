480be7f4cf082e626e544e36bf0b9d7c41277704
Vrivas99 <vi.rivas@duocuc.cl>
2023-06-22 19:26:32 -0400

    fix mostrar y ocultar detalles de compra

--- pages/historial.js
+++ pages/historial.js
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "@/components/Navbar";
 import { getPagos, getPagitos } from "@/services/itemService";
-
-//ya no se estan usando
-import Pagitos from "@/components/Pagitos";
-import Tablahistorial from "@/components/Tablahistorial";
 import axios from "axios";
 
 const historial = ({ pagos, detalle }) => {
@@ -13,25 +9,26 @@ const historial = ({ pagos, detalle }) => {
   // Estado para almacenar las filas expandidas
   const [expandedRows, setExpandedRows] = useState([]);
 
-  // Función para manejar la expansión de una fila
-  const handleExpandRow = (row) => {
-    if (expandedRows.includes(row)) {
-      {
-        /* Filas adicionales si la fila principal está expandida */
-      }
-      setExpandedRows(expandedRows.filter((r) => r !== row));
-    } else {
-      {
-        /* Si la fila no está expandida, la agregamos al estado de filas expandidas*/
-      }
-      setExpandedRows([...expandedRows, row]);
-    }
-  };
+  // Función para manejar el click en una fila
+  const handleRowClick = (index) => {
 
-  // Función para verificar si una fila está expandida
-  const isRowExpanded = (row) => {
-    return expandedRows.includes(row);
+    const updatedRows = [...expandedRows];
+    const rowIndex = updatedRows.indexOf(index);
+    // Si la fila está expandida, la eliminamos del array
+    if (rowIndex !== -1) {
+
+      updatedRows.splice(rowIndex, 1);
+      // Si no está expandida, la agregamos al array
+    } else {
+      updatedRows.push(index);
+    }
+    // Actualizamos el estado
+    setExpandedRows(updatedRows);
   };
+  // Función para determinar si una fila está expandida
+  const isRowExpanded = (index) => expandedRows.includes(index);
+
+
   const getProfile = async () => {
     try {
       const response = await axios.get('http://localhost:3000/api/profile')
@@ -52,109 +49,105 @@ const historial = ({ pagos, detalle }) => {
 
 
   return (
+
     <div>
       <div>
         <Navbar />
       </div>
-      <div className="m-11 relative overflow-x-auto sm:rounded-xl">
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-black dark:text-gray-400">
+      <div className="m-10 overflow-x-auto rounded-t-lg">
+        <table className="w-full text-sm text-gray-500">
+          <thead className="text-xs text-gray-800 uppercase bg-black">
             <tr>
-              <th scope="col" className="text-white px-6 py-3 text-center">
+              <th scope="col" className="text-white md:w-[30%]  py-4 text-center">
                 ORDEN DE COMPRA
               </th>
-              <th scope="col" className="text-white px-6 py-3 text-center">
+              <th scope="col" className="text-white md:w-[30%] overflow-x-auto  py-4 text-center">
                 PRECIO
               </th>
-              <th scope="col" className="text-white px-6 py-3 text-center">
+              <th scope="col" className="text-white md:w-[30%]  py-4 text-center">
                 FECHA DE COMPRA
               </th>
-              <th scope="col" className="text-white px-6 py-3 text-center">
+              <th scope="col" className="text-white md:w-[10%]  py-4 text-center">
                 Detalles
               </th>
             </tr>
           </thead>
           <tbody>
             {/* Filas principales */}
-            {filtroPago && filtroPago.map((pago) => (
-              <>
-                <tr className="border bg-white border-gray-700 hover:bg-gray-200 hover:text-white">
-                  <td
-                    scope="row"
-                    className="px-4 py-1 font-medium text-black text-center"
-                  >
-                    {pago.buyOrder}
-                  </td>
-                  <td
-                    scope="row"
-                    className="text-center px-4 py-1 font-medium text-black"
-                  >
-                    ${pago.PRECIO}
-                  </td>
-                  <td
-                    scope="row"
-                    className="text-center px-4 py-1 font-medium text-black"
-                  >
-                    {pago.fecha}
-                  </td>
-                  <td
-                    scope="row"
-                    className="text-center px-4 py-1 font-medium text-black"
-                  >
-                    <button
-                      className="text-purple-500 hover:underline focus:outline-none"
-                      onClick={() => handleExpandRow(0)}
+            {filtroPago &&
+              filtroPago.map((pago, index) => (
+                <React.Fragment key={index}>
+                  <tr className="border bg-white border-gray-700 hover:bg-gray-200 hover:text-white">
+                    <td
+                      scope="row"
+                      className="md:w-[30%]  py-1 font-medium text-black text-center"
                     >
-                      {isRowExpanded(0) ? "- Detalles" : "+ Detalles"}
-                    </button>
-                  </td>
-                </tr>
-                {isRowExpanded(0) && (
-                  <>
-                    {/* Fila con encabezados adicionales */}
-                    <tr className="border bg-gray-300">
-                      <th className="px-4 py-2 text-black font-bold text-center">
-                        ID
-                      </th>
-                      <th className="px-4 py-2  text-black font-bold text-center">
-                        NOMBRE
-                      </th>
-                      <th className="px-4 py-2 text-black font-bold text-center">
-                        CANTIDAD
-                      </th>
-                      <th className="px-4 py-2 text-black font-bold text-center">
-                        PRECIO
-                      </th>
-                    </tr>
-                    {/* Fila con información adicional */}
-
-                    {
-                      detalle.filter(det => det.bOr === pago.buyOrder).map((det) => (
-                        <tr className="border bg-gray-200">
-                          <td className="px-4 py-2 text-gray-700  text-center">
-                            {det.ide}
-                          </td>
-                          <td className="px-4 py-2 text-gray-700  text-center">
-                            {det.titulo}
-                          </td>
-                          <td className="px-4 py-2 text-gray-700  text-center">
-                            {det.qty}
-                          </td>
-                          <td className="px-4 py-2 text-gray-700  text-center">
-                            {det.precio}
-                          </td>
-                        </tr>
-                      ))
-                    }
-                  </>
-                )}
-              </>
-
-            ))}
-            {/* TODO ESTO DEBERIA ESTAR POR CADA ORDEN DE COMPRA*/}
-            {/* SOLO SE RENDERIZA LOS PRODUCTOS DE LA PRIMERA ORDEN DE COMPRA */}
-
-            {/* Filas adicionales si la fila principal está expandida */}
+                      {pago.buyOrder}
+                    </td>
+                    <td
+                      scope="row"
+                      className="text-center md:w-[30%] overflow-x-auto  py-1 font-medium text-black"
+                    >
+                      ${pago.PRECIO}
+                    </td>
+                    <td
+                      scope="row"
+                      className="text-center md:w-[30%]  py-1 font-medium text-black"
+                    >
+                      {pago.fecha}
+                    </td>
+                    <td
+                      scope="row"
+                      className="text-center md:w-[10%]  py-1 font-medium text-black"
+                    >
+                      <button
+                        className="text-purple-500 hover:underline focus:outline-none"
+                        onClick={() => handleRowClick(index)}
+                      >
+                        {isRowExpanded(index) ? 'Ocultar' : 'Mostrar'}
+                      </button>
+                    </td>
+                  </tr>
+                  {isRowExpanded(index) && (
+                    <React.Fragment>
+                      {/* Fila con encabezados adicionales */}
+                      <tr className="border bg-gray-300">
+                        <th className="md:w-[30%]  py-1 text-black font-bold text-center">
+                          ID
+                        </th>
+                        <th className="md:w-[30%] overflow-x-auto  py-1 text-black font-bold text-center">
+                          NOMBRE
+                        </th>
+                        <th className="md:w-[30%]  py-1 text-black font-bold text-center">
+                          CANTIDAD
+                        </th>
+                        <th className="md:w-[10%]  py-1 text-black font-bold text-center">
+                          PRECIO
+                        </th>
+                      </tr>
+                      {/* Fila con información adicional */}
+                      {detalle
+                        .filter((det) => det.bOr === pago.buyOrder)
+                        .map((det, subIndex) => (
+                          <tr className="border bg-gray-200" key={subIndex}>
+                            <td className="md:w-[30%]  py-3 text-gray-700 text-center">
+                              {det.ide}
+                            </td>
+                            <td className="md:w-[30%] py-3 text-gray-700 text-center text-ellipsis truncate">
+                              {det.titulo}
+                            </td>
+                            <td className="md:w-[30%]  py-3 text-gray-700 text-center">
+                              {det.qty}
+                            </td>
+                            <td className="md:w-[10%]  py-3 text-gray-700 text-center">
+                              {det.precio}
+                            </td>
+                          </tr>
+                        ))}
+                    </React.Fragment>
+                  )}
+                </React.Fragment>
+              ))}
           </tbody>
         </table>
       </div>
@@ -164,14 +157,24 @@ const historial = ({ pagos, detalle }) => {
 
 export default historial
 
-export async function getStaticProps() {
+export async function getServerSideProps(context) {
+  try {
+    const res = await getPagos();
+    const res2 = await getPagitos();
 
-  const res = await getPagos();
-  const res2 = await getPagitos();
-  return {
-    props: {
-      pagos: res,
-      detalle: res2,
-    },
-  };
+    return {
+      props: {
+        pagos: res,
+        detalle: res2,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return {
+      props: {
+        pagos: null,
+        detalle: null,
+      },
+    };
+  }
 }
